2beb3dbf49b69f3373079533e96dd6d0105a126e
Jhonatan <jhonatanhuaman76@gmail.com>
2026-03-02 20:12:01 -0500

    initial commit

---
+++ backend/src/main/java/com/mibanco/apigastos/ApiGestionDeGatosApplication.java
@@ -0,0 +1,13 @@
+package com.mibanco.apigastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionDeGatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionDeGatosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mibanco/apigastos/ApiGestionDeGatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibanco.apigastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionDeGatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
